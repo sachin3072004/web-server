b3e63874c112a6bb6eac21e92acabdf702c7e2e9
Ubuntu <ubuntu@ip-172-31-82-30.ec2.internal>
2025-01-10 01:54:17 +0000

    Initial Code

--- rust-web-server/src/main.rs
+++ rust-web-server/src/main.rs
@@ -1,3 +1,13 @@
+struct Server{
+    address:String,
+    port: i32
+}
+
+impl Server{
+    fn new(s:String)->Server{
+        Server{address:s,port:p}
+    }
+}
 fn main() {
-    println!("Hello, world!");
+        let mut s = Server::new(String::from("127.0.0.1:8080"));
 }
